bbfc9f7a99169047661996189a48679808f00802
NgHieuNTN <69446134+NgHieuNTN@users.noreply.github.com>
2020-08-27 14:08:02 +0700

    Add files via upload

--- Read_curent_Node.ino
+++ Read_curent_Node.ino
@@ -1,16 +1,23 @@
 #include <SoftwareSerial.h>
 #include <PZEM004Tv30.h>
-
+#include <avr/pgmspace.h>
 
 #define M0 12
 #define M1 13
-#define Tx1 11
-#define Rx1 10
+#define Tx1 2
+#define Rx1 3
 
-PZEM004Tv30 pzem(9, 8);   //tx,rx
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+PZEM004Tv30 pzem(9, 8);   //rx,tx
 PZEM004Tv30 pzem1(7, 6);
 PZEM004Tv30 pzem2(5, 4);
-SoftwareSerial LoRa(Tx1, Rx1);
+SoftwareSerial LoRa(Rx1, Tx1);
 
 float voltage[3]  = {0.0};
 float current[3]  = {0.0};
@@ -20,12 +27,12 @@ float freq[3]  = {0.0};
 float pf[3]  = {0.0};
 
 
-boolean send = 0;
+boolean send = true;
 //boolean demo = true;
 boolean demo = false;
 
 unsigned long timeOld = 0;
-unsigned long timeInterval = 1000; // mili second
+unsigned long timeInterval = 5000; // mili second
 
 void setup() {
   Serial.begin(9600);
@@ -43,45 +50,28 @@ void setup() {
 void readData(PZEM004Tv30 pzemTemp, int device) {
   if (demo == false) {
 
+    Serial.print("Ram free is: ");
+    Serial.println(freeMemory());
     voltage[device] = pzemTemp.voltage();
-    if (voltage[device] != NAN) {
-      //Serial.print("Voltage: "); Serial.print(voltage[device]); Serial.println("V");
-    } else {
-      Serial.println("Error reading voltage");
-    }
+    Serial.println("Read 2");
+    delay(10);
     current[device] = pzemTemp.current();
-    if (current[device] != NAN) {
-      //Serial.print("Current: "); Serial.print(current[device]); Serial.println("A");
-    } else {
-      Serial.println("Error reading current");
-    }
+    Serial.println("Read 3");
+    delay(10);
     power[device] = pzemTemp.power();
-    if (current[device] != NAN) {
-      //Serial.print("Power: "); Serial.print(power[device]); Serial.println("W");
-    } else {
-      Serial.println("Error reading power");
-    }
+    Serial.println("Read 4");
+    delay(10);
     energy[device] = pzemTemp.energy();
-    if (current[device] != NAN) {
-      //Serial.print("Energy: "); Serial.print(energy[device], 3); Serial.println("kWh");
-    } else {
-      Serial.println("Error reading energy");
-    }
+    Serial.println("Read 5");
+    delay(10);
     freq[device] = pzemTemp.frequency();
-    if (current[device] != NAN) {
-      //Serial.print("Frequency: "); Serial.print(freq[device], 1); Serial.println("Hz");
-    } else {
-      Serial.println("Error reading frequency");
-    }
+    Serial.println("Read 6");
+    delay(10);
     pf[device] = pzemTemp.pf();
-    if (current[device] != NAN) {
-      //Serial.print("PF: "); Serial.println(pf[device]);
-    } else {
-      Serial.println("Error reading power factor");
-    }
-    Serial.println();
+    Serial.println("Read 7");
+    //delay(10);
   } else {
-    //    Serial.println("Demo Mode, all = random");
+    //    //Serial.println("Demo Mode, all = random");
     //    voltage  = random(190, 300);
     //    current  = random(0, 100);
     //    power  = random(0, 10000);
@@ -93,20 +83,23 @@ void readData(PZEM004Tv30 pzemTemp, int device) {
 
 void loop() {
 
-  unsigned long timeNow = millis();
+  Serial.println("step 1");
 
-  if (timeNow > timeOld) {
-    if (timeNow - timeOld > timeInterval) {
+  if (millis() >= timeOld) {
+    if (millis() - timeOld > timeInterval) {
       send  = true;
     }
+    Serial.println("step 2");
   } else {
-    if (timeOld - timeNow > timeInterval) {
-      send  = true;
+    Serial.println("step 3");
+    if (timeOld - millis() > timeInterval) {
+      send  = true;;
     }
   }
 
 
   if (send == true) {
+    Serial.println("Send");
 
     readData(pzem, 0);
     readData(pzem1, 1);
@@ -120,9 +113,16 @@ void loop() {
     dataSend += String(power[0]);
     dataSend += "\",\"frequency\":\"";
     dataSend += String(freq[0]);
-    dataSend += "\"";
+    dataSend += "\",\"energy\":\"";
+    dataSend += String(energy[0]);
+    dataSend += "\",\"pf\":\"";
+    dataSend += String(pf[0]);
+    dataSend += "\"}";
+    LoRa.println(dataSend);
+    Serial.println("OK1");
 
-    dataSend += ",\"voltage1\":\"";
+    dataSend = "";
+    dataSend = "{\"voltage1\":\"";
     dataSend += String(voltage[1]);
     dataSend += "\",\"current1\":\"";
     dataSend += String(current[1]);
@@ -130,9 +130,16 @@ void loop() {
     dataSend += String(power[1]);
     dataSend += "\",\"frequency1\":\"";
     dataSend += String(freq[1]);
-    dataSend += "\"";
+    dataSend += "\",\"energy1\":\"";
+    dataSend += String(energy[1]);
+    dataSend += "\",\"pf1\":\"";
+    dataSend += String(pf[1]);
+    dataSend += "\"}";
+    LoRa.println(dataSend);
+    Serial.println("OK2");
 
-    dataSend += ",\"voltage2\":\"";
+    dataSend = "";
+    dataSend = "{\"voltage2\":\"";
     dataSend += String(voltage[2]);
     dataSend += "\",\"current2\":\"";
     dataSend += String(current[2]);
@@ -140,11 +147,26 @@ void loop() {
     dataSend += String(power[2]);
     dataSend += "\",\"frequency2\":\"";
     dataSend += String(freq[2]);
+    dataSend += "\",\"energy2\":\"";
+    dataSend += String(energy[2]);
+    dataSend += "\",\"pf2\":\"";
+    dataSend += String(pf[2]);
     dataSend += "\"}";
     LoRa.println(dataSend);
-    Serial.println(dataSend);
     dataSend = "";
     timeOld = millis();
+    Serial.println("Send done!");
   }
   send  = false;
+}
+
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
 }
